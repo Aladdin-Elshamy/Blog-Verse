ba0a05f56c5b108bada8a169c5002225ddade3c9
Aladdin Elshamy <aladdinelshamy@gmail.com>
2025-08-06 12:14:13 +0300

    refactor: simplify middleware configuration by removing unnecessary options

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,7 @@ const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 
-app.use(express.json({ limit: "50mb" }));
-
-app.use(
-    cors({
-        origin: "*",
-        // credentials: true,
-    })
-);
+app.use(express.json());
 
 app.use('/users', userRoutes)
 app.use("/blogs", blogRoutes);
