034d5e9d5e06c09ed72cd01636b8a5ebd7fc4172
Aladdin Elshamy <aladdinelshamy@gmail.com>
2025-08-06 12:21:32 +0300

    refactor: update API endpoints to use relative paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy: {
+      '/blogs': {
+        target: 'http://localhost:3000',
+      },
+      '/users': {
+        target: 'http://localhost:3000',
+      },
+    }
+  }
 })
